809f9360bc0937f23d3b84032959f7827f0a779a
julian-nec-ui <julian.neacsu@gmail.com>
2026-05-03 15:34:09 -0500

    updates-5

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
-          src="/deploy-to-git/next.svg"
+          src="next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
@@ -43,8 +43,9 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/deploy-to-git/vercel.svg"
+              src="vercel.svg"
               alt="Vercel logomark"
+              style={{ width: '25%', height: '25%' }} 
               width={16}
               height={16}
             />
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  basePath: '/deploy-to-git',
-  assetPrefix: '/deploy-to-git/' // Ensures assets load from the subfolder
+  // basePath: '/deploy-to-git',
+  // assetPrefix: '/deploy-to-git' // Ensures assets load from the subfolder
 }
 
 export default nextConfig
