0dce7e8cfa5470b1fee1c86a402766206774d815
julian-nec-ui <julian.neacsu@gmail.com>
2026-05-03 21:58:59 -0500

    updates-6

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { ToastContainer, toast } from 'react-toastify';
 
 export default function Home() {
-  const notify = () => toast('hello');
+  const notify = (message: string, size: number) => toast(message, {style: {width: size ? size + "px": "725px"}});
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -41,22 +41,6 @@ export default function Home() {
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-39.5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="vercel.svg"
-              alt="Vercel logomark"
-              style={{ width: '25%', height: '25%' }}
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/2 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-39.5"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
@@ -65,7 +49,7 @@ export default function Home() {
             Documentation
           </a>
           <button
-            onClick={notify}
+            onClick={() => notify("This is an alert at : " + new Date().toUTCString(), 725)}
             className="relative inline-flex items-center
             justify-center p-0.5 bg-linear-to-r
             from-cyan-500 to-blue-500 rounded-xl
@@ -79,20 +63,111 @@ export default function Home() {
               className="relative px-4 py-2.5 transition-all 
               ease-in duration-125 bg-slate-900 
               rounded-xl hover:bg-transparent
-              hover:dark:bg-transparent leading-5"
+              hover:dark:bg-transparent leading-5 font-bold"
             >
-              Purple to blue
+              Check Alert !
             </span>
-            
           </button>
           <ToastContainer
-              position="top-left"
-              autoClose={1000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              draggable
-            />
+            position="top-left"
+            autoClose={1000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            draggable
+          />
+        </div>
+        <div
+          id="alert-additional-content-1"
+          className="p-4 mb-4 mt-7 text-sm text-teal-600 rounded-xl bg-slate-950 border border-teal-800"
+          role="alert"
+        >
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <svg
+                className="w-5 h-5 shrink-0 me-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+              <span className="sr-only">Info</span>
+              <h3 className="font-medium">This is a info alert</h3>
+            </div>
+            <button
+              type="button"
+              data-dismiss-target="#alert-additional-content-1"
+              aria-label="Close"
+              className="ms-auto -mx-1.5 -my-1.5 rounded-lg 
+              focus:ring-2 focus:cyan-600 hover:border-2 text-[#dd0000]
+              inline-flex items-center justify-center h-7 w-7 shrink-0"
+              onClick={() => notify("This is a message alert X " + new Date().toUTCString(), 800)}
+            >
+              <span className="sr-only">Close</span>
+              <svg
+                className="w-4 h-4"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  stroke="#dd0000"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="3"
+                  d="M6 18 17.94 6M18 18 6.06 6"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="mt-2 mb-4">
+            More info about this info alert goes here. This example text is
+            going to run a bit longer so that you can see how spacing within an
+            alert works with this kind of content.
+          </div>
+          <button
+            type="button"
+            className="inline-flex items-center text-slate-200
+            bg-transparent hover:bg-slate-700 box-border border
+            border-teal-800 focus:ring-2 focus:ring-teal-600 
+            shadow-xs font-medium leading-5 rounded-md text-sm
+            px-3 py-1.5 focus:outline-none"
+          >
+            <svg
+              className="w-4 h-4 me-1.5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                strokeWidth="2"
+                d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z"
+              />
+              <path
+                stroke="currentColor"
+                strokeWidth="2"
+                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+            View more
+          </button>
         </div>
       </main>
     </div>
